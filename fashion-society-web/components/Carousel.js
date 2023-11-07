3fcb8bb44522e8f45bf594f2ceec2cba77c0d841
jaden416 <jaden416@outlook.com>
2023-11-07 13:24:34 -0500

    carousel

---
+++ fashion-society-web/components/Carousel.js
@@ -0,0 +1,14 @@
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+
+const Carousel = () => {
+
+  
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Carousel
